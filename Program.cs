9b1614ec053aca7a878da7e259a386ea52919b64
Gabriel Ferreira Sasdelli <sasdelle69@gmail.com>
2022-08-17 22:05:49 -0300

    numeros pratos

--- Program.cs
+++ Program.cs
@@ -13,45 +13,45 @@ namespace ListaPessoas
             List<Pratos> pratos = new List<Pratos>();
 
 
-            pratos.Add(new Pratos("", "", "", "",""));
+            pratos.Add(new Pratos("Prato-1", "Ingredientes-1", "Descrição-1", "URL-1","Tempo de Preparo-1"));
 
-            pratos.Add(new Pratos("", "", "", "", ""));
+            pratos.Add(new Pratos("Prato-2", "Ingredientes-2", "Descrição-2", "URL-2", "Tempo de Preparo-2"));
 
-            pratos.Add(new Pratos("", "", "", "", ""));
+            pratos.Add(new Pratos("Prato-3", "Ingredientes-3", "Descrição-3", "URL-3", "Tempo de Preparo-3"));
 
-            pratos.Add(new Pratos("", "", "", "", ""));
+            pratos.Add(new Pratos("Prato-4", "Ingredientes-4", "Descrição-4", "URL-4", "Tempo de Preparo-4"));
 
-            pratos.Add(new Pratos("", "", "", "", ""));
+            pratos.Add(new Pratos("Prato-5", "Ingredientes-5", "Descrição-5", "URL-5", "Tempo de Preparo-5"));
 
-            pratos.Add(new Pratos("", "", "", "", ""));
+            pratos.Add(new Pratos("Prato-6", "Ingredientes-6", "Descrição-6", "URL-6", "Tempo de Preparo-6"));
 
-            pratos.Add(new Pratos("", "", "", "", ""));
+            pratos.Add(new Pratos("Prato-7", "Ingredientes-7", "Descrição-7", "URL-7", "Tempo de Preparo-7"));
 
-            pratos.Add(new Pratos("", "", "", "", ""));
+            pratos.Add(new Pratos("Prato-8", "Ingredientes-8", "Descrição-8", "URL-8", "Tempo de Preparo-8"));
 
-            pratos.Add(new Pratos("", "", "", "", ""));
+            pratos.Add(new Pratos("Prato-9", "Ingredientes-9", "Descrição-9", "URL-9", "Tempo de Preparo-9"));
 
-            pratos.Add(new Pratos("", "", "", "", ""));
+            pratos.Add(new Pratos("Prato-10", "Ingredientes-10", "Descrição-10", "URL-10", "Tempo de Preparo-10"));
 
-            pratos.Add(new Pratos("", "", "", "", ""));
+            pratos.Add(new Pratos("Prato-11", "Ingredientes-11", "Descrição-11", "URL-11", "Tempo de Preparo-11"));
 
-            pratos.Add(new Pratos("", "", "", "", ""));
+            pratos.Add(new Pratos("Prato-12", "Ingredientes-12", "Descrição-12", "URL-12", "Tempo de Preparo-12"));
 
-            pratos.Add(new Pratos("", "", "", "", ""));
+            pratos.Add(new Pratos("Prato-13", "Ingredientes-13", "Descrição-13", "URL-13", "Tempo de Preparo-13"));
 
-            pratos.Add(new Pratos("", "", "", "", ""));
+            pratos.Add(new Pratos("Prato-14", "Ingredientes-14", "Descrição-14", "URL-14", "Tempo de Preparo-14"));
 
-            pratos.Add(new Pratos("", "", "", "", ""));
+            pratos.Add(new Pratos("Prato-15", "Ingredientes-15", "Descrição-15", "URL-15", "Tempo de Preparo-15"));
 
-            pratos.Add(new Pratos("", "", "", "", ""));
+            pratos.Add(new Pratos("Prato-16", "Ingredientes-16", "Descrição-16", "URL-16", "Tempo de Preparo-16"));
 
-            pratos.Add(new Pratos("", "", "", "", ""));
+            pratos.Add(new Pratos("Prato-17", "Ingredientes-17", "Descrição-17", "URL-17", "Tempo de Preparo-17"));
 
-            pratos.Add(new Pratos("", "", "", "", ""));
+            pratos.Add(new Pratos("Prato-18", "Ingredientes-18", "Descrição-18", "URL-18", "Tempo de Preparo-18"));
 
-            pratos.Add(new Pratos("", "", "", "", ""));
+            pratos.Add(new Pratos("Prato-19", "Ingredientes-19", "Descrição-19", "URL-19", "Tempo de Preparo-19"));
 
-            pratos.Add(new Pratos("", "", "", "", ""));
+            pratos.Add(new Pratos("Prato-20", "Ingredientes-20", "Descrição-20", "URL-20", "Tempo de Preparo-20"));
         }
     }
 }
