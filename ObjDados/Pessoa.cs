b7bd9f5ceb855b720d29422c7ab3af3976ff18cf
Gabriel Ferreira Sasdelli <sasdelle69@gmail.com>
2022-08-17 21:51:05 -0300

    Adicionar arquivos de projeto.

---
+++ ObjDados/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListaPessoas.ObjDados
+{
+    public class Pratos
+    {
+        public Pratos() { }
+        public string NomePrato { get; set; }
+        public string Ingredientes { get; set; }
+        public string DescricaoPrato  { get; set; }
+        public string URLFoto { get; set; }
+        public string TempoPreparo { get; set; }
+
+        public Pratos (string nomeprato, string ingredientes, string descricaoprato, string urlfoto, string tempopreparo)
+        {
+            this.NomePrato = nomeprato;
+            this.Ingredientes = ingredientes;
+            this.DescricaoPrato = descricaoprato;
+            this.URLFoto = urlfoto;
+            this.TempoPreparo = tempopreparo;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using ListaPessoas.ObjDados;
+using System;
+using System.Collections.Generic;
+
+namespace ListaPessoas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Menu de Pratos");
+
+            List<Pratos> pratos = new List<Pratos>();
+
+
+            pratos.Add(new Pratos("", "", "", "",""));
+
+            pratos.Add(new Pratos("", "", "", "", ""));
+
+            pratos.Add(new Pratos("", "", "", "", ""));
+
+            pratos.Add(new Pratos("", "", "", "", ""));
+
+            pratos.Add(new Pratos("", "", "", "", ""));
+
+            pratos.Add(new Pratos("", "", "", "", ""));
+
+            pratos.Add(new Pratos("", "", "", "", ""));
+
+            pratos.Add(new Pratos("", "", "", "", ""));
+
+            pratos.Add(new Pratos("", "", "", "", ""));
+
+            pratos.Add(new Pratos("", "", "", "", ""));
+
+            pratos.Add(new Pratos("", "", "", "", ""));
+
+            pratos.Add(new Pratos("", "", "", "", ""));
+
+            pratos.Add(new Pratos("", "", "", "", ""));
+
+            pratos.Add(new Pratos("", "", "", "", ""));
+
+            pratos.Add(new Pratos("", "", "", "", ""));
+
+            pratos.Add(new Pratos("", "", "", "", ""));
+
+            pratos.Add(new Pratos("", "", "", "", ""));
+
+            pratos.Add(new Pratos("", "", "", "", ""));
+
+            pratos.Add(new Pratos("", "", "", "", ""));
+
+            pratos.Add(new Pratos("", "", "", "", ""));
+        }
+    }
+}
